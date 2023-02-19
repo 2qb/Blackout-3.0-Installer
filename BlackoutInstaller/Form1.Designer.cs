9b8c46702ae31b60a3b29b1283485b40b464dabd
2qb <dumblinglmfao@gmail.com>
2023-02-19 18:32:02 +0100

    Add project files.

---
+++ BlackoutInstaller/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace BlackoutInstaller
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.DirectoryText = new System.Windows.Forms.TextBox();
+            this.InstallButton = new System.Windows.Forms.Button();
+            this.DirButton = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.install_label = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(161)))), ((int)(((byte)(0)))), ((int)(((byte)(255)))));
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(161)))), ((int)(((byte)(0)))), ((int)(((byte)(255)))));
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.ForeColor = System.Drawing.Color.Snow;
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.XButton_MouseClick);
+            // 
+            // DirectoryText
+            // 
+            this.DirectoryText.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(33)))), ((int)(((byte)(33)))));
+            this.DirectoryText.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.DirectoryText.ForeColor = System.Drawing.Color.Silver;
+            resources.ApplyResources(this.DirectoryText, "DirectoryText");
+            this.DirectoryText.Name = "DirectoryText";
+            // 
+            // InstallButton
+            // 
+            this.InstallButton.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(161)))), ((int)(((byte)(0)))), ((int)(((byte)(255)))));
+            resources.ApplyResources(this.InstallButton, "InstallButton");
+            this.InstallButton.ForeColor = System.Drawing.Color.White;
+            this.InstallButton.Name = "InstallButton";
+            this.InstallButton.UseVisualStyleBackColor = true;
+            this.InstallButton.MouseClick += new System.Windows.Forms.MouseEventHandler(this.InstallButton_MouseClick);
+            // 
+            // DirButton
+            // 
+            this.DirButton.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(161)))), ((int)(((byte)(0)))), ((int)(((byte)(255)))));
+            resources.ApplyResources(this.DirButton, "DirButton");
+            this.DirButton.ForeColor = System.Drawing.Color.Silver;
+            this.DirButton.Name = "DirButton";
+            this.DirButton.UseVisualStyleBackColor = true;
+            this.DirButton.Click += new System.EventHandler(this.DirButton_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
+            this.panel1.Controls.Add(this.button1);
+            resources.ApplyResources(this.panel1, "panel1");
+            this.panel1.Name = "panel1";
+            this.panel1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Top_MouseDown);
+            this.panel1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Top_MouseMove);
+            this.panel1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Top_MouseUp);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::BlackoutInstaller.Properties.Resources.banner;
+            resources.ApplyResources(this.pictureBox1, "pictureBox1");
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.TabStop = false;
+            // 
+            // install_label
+            // 
+            resources.ApplyResources(this.install_label, "install_label");
+            this.install_label.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.install_label.Name = "install_label";
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(33)))), ((int)(((byte)(33)))));
+            this.Controls.Add(this.install_label);
+            this.Controls.Add(this.DirectoryText);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.DirButton);
+            this.Controls.Add(this.InstallButton);
+            this.Controls.Add(this.pictureBox1);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Opacity = 0D;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox DirectoryText;
+        private System.Windows.Forms.Button InstallButton;
+        private System.Windows.Forms.Button DirButton;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label install_label;
+    }
+}
+
---
+++ BlackoutInstaller/Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Windows.Forms;
+using Newtonsoft.Json;
+using System.Diagnostics;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Window;
+
+
+namespace BlackoutInstaller
+{
+
+    public partial class Form1 : Form
+    {
+
+        private bool dragging = false;
+        private Point lastLocation;
+
+        Timer fadeinTimer = new Timer();
+        Timer fadeoutTimer = new Timer();
+
+        public Form1()
+        {
+            InitializeComponent();
+            string defaultDirectory = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), ".minecraft");
+            DirectoryText.Text = defaultDirectory;
+        }
+
+        void fadeIn(object sender, EventArgs e)
+        {
+            if (Opacity >= 1)
+            {
+                fadeinTimer.Stop();
+            }
+            else
+                Opacity += 0.05;
+        }
+
+        void fadeOut(object sender, EventArgs e)
+        {
+            if (Opacity <= 0)
+            {
+                fadeoutTimer.Stop();
+                Close();
+            }
+            else
+                Opacity -= 0.05;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Opacity = 0;
+
+            fadeinTimer.Interval = 30;
+            fadeinTimer.Tick += new EventHandler(fadeIn);
+            fadeinTimer.Start();
+        }
+
+        private void XButton_MouseClick(object sender, MouseEventArgs e)
+        {
+            fadeoutTimer.Interval = 30;
+            fadeoutTimer.Tick += new EventHandler(fadeOut);
+            fadeoutTimer.Start();
+        }
+
+        private void Top_MouseDown(object sender, MouseEventArgs e)
+        {
+            dragging = true;
+            lastLocation = e.Location;
+        }
+
+        private void Top_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (dragging && e.Y < 30)
+            {
+                this.Location = new Point(
+                    (this.Location.X - lastLocation.X) + e.X,
+                    (this.Location.Y - lastLocation.Y) + e.Y);
+                this.Update();
+            }
+
+        }
+        private void Top_MouseUp(object sender, MouseEventArgs e)
+        {
+            dragging = false;
+        }
+
+
+        private void DirButton_Click(object sender, EventArgs e)
+        {
+            using (var dialog = new FolderBrowserDialog())
+            {
+                dialog.SelectedPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData));
+                DialogResult result = dialog.ShowDialog();
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(dialog.SelectedPath))
+                {
+                    DirectoryText.Text = dialog.SelectedPath;
+                }
+            }
+        }
+
+        private void InstallButton_MouseClick(object sender, MouseEventArgs e) {
+            string apiUrl = $"https://api.github.com/repos/chell-dev/Blackout-3.0/releases/latest";
+            string fabricInstallerUrl = "https://maven.fabricmc.net/net/fabricmc/fabric-installer/0.11.1/fabric-installer-0.11.1.jar";
+            string fabricInstallTo = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\Downloads";
+
+            using (WebClient client = new WebClient()) 
+            {
+                client.Headers.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64)");
+                string releaseJson = client.DownloadString(apiUrl);
+
+                dynamic release = JsonConvert.DeserializeObject(releaseJson);
+                string downloadUrl = release.assets[0].browser_download_url;
+
+                using (WebClient downloadClient = new WebClient()) 
+                {
+                    string filename = downloadUrl.Split('/').Last();
+                    string installto = DirectoryText.Text;
+                    string downloadPath = Path.Combine(installto, filename);
+
+                    DialogResult fabricresult = MessageBox.Show("Do you have fabric 1.19.3 installed?", "Blackout 3.0", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                    if (fabricresult == DialogResult.No)
+                    {
+                        downloadClient.DownloadFile(fabricInstallerUrl, fabricInstallTo);
+
+                        ProcessStartInfo fabricJar = new ProcessStartInfo();
+                        fabricJar.FileName = "java";
+                        fabricJar.Arguments = "-jar \"" + fabricInstallTo + "\"";
+                        Process fabricProcess = Process.Start(fabricJar);
+                        fabricProcess.WaitForExit();
+
+                    }
+
+                    if (File.Exists(Path.Combine(installto, "mods", filename)))
+                    {
+                        DialogResult result = MessageBox.Show("Blackout is already installed. Do you want to update it?", "Blackout 3.0" , MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                        if (result == DialogResult.No) 
+                        {
+                            return;
+                        }
+                        else {
+                            File.Delete(Path.Combine(installto, "mods", filename)); 
+                        }
+                    }
+                    downloadClient.DownloadFile(downloadUrl, downloadPath);
+
+                        string modsPath = Path.Combine(installto, "mods");
+                        Directory.CreateDirectory(modsPath);
+
+                        string destinationPath = Path.Combine(modsPath, filename);
+                        File.Move(downloadPath, destinationPath);
+
+                    install_label.Text = "Installation successfull!";
+                }
+            }
+        }
+    }
+}
